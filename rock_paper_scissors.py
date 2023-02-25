bce350f2597e4f78c7d21850ef4af26b131a4c89
Roy <royyuan19@gmail.com>
2023-02-24 23:10:51 -0800

    first commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,13 @@
+
+#greet user
+import random
+
+
+print("Hi there.")
+print()
+rules = "Here are the rules: two players secretly pick one of \"rock,\" \"paper,\"or \"scissors.\" \
+Both players reveal their selection to the other player at once; the winner is chosen \
+based on what the selections are. Rock beats scissors (by crushing them); scissors\
+beats paper (by cutting it); and paper beats rock (by covering it). If both players\
+select the same one, it is a tie"
+
